39825f2d391a071ae2093b0c2ea381608595d9d3
Bhagyashree <bgshettigar95@gmail.com>
2025-11-12 23:07:28 +0530

    Addded location handler

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,8 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Text, View } from "react-native";
 import WheatherInfo from "../components/WheatherInfo";
+import {
+  getCurrentPositionAsync,
+  PermissionStatus,
+  useForegroundPermissions,
+} from "expo-location";
+// import Config from "react-native-config";
+
+// console.log(Config.API_URL);
 
 const Home = () => {
+  const [loactionPermission, requestLocationPermission] =
+    useForegroundPermissions();
+  const [pickedLocation, setPickedLocation] = useState();
+
+  const verifyLocationPermission = async () => {
+    if (loactionPermission.status === PermissionStatus.UNDETERMINED) {
+      const permission = await requestLocationPermission();
+
+      return permission.granted;
+    }
+
+    if (loactionPermission.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        "Not sufficient access",
+        " Please grant access to location to use this feature"
+      );
+      return false;
+    }
+
+    return true;
+  };
+
+  const locateUserHandler = async () => {
+    const hasPermission = await verifyLocationPermission();
+
+    if (!hasPermission) {
+      return;
+    }
+
+    const location = await getCurrentPositionAsync();
+
+    setPickedLocation({
+      lat: location.coords.latitude,
+      lng: location.coords.longitude,
+    });
+  };
+
+  const getWheatherForecast = async () => {};
+
+  useEffect(() => {
+    locateUserHandler();
+  }, []);
+
   return (
     <View>
       <Text>Home</Text>
